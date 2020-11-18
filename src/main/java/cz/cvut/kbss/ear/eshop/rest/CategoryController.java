c832a22a254d06b525b8e33b5d094ba58ca3f2c2
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-11-18 08:20:05 +0100

    [Solution] Seminar 08 task solution.

--- src/main/java/cz/cvut/kbss/ear/eshop/rest/CategoryController.java
+++ src/main/java/cz/cvut/kbss/ear/eshop/rest/CategoryController.java
@@ -1,11 +1,26 @@
 package cz.cvut.kbss.ear.eshop.rest;
 
+import cz.cvut.kbss.ear.eshop.exception.NotFoundException;
+import cz.cvut.kbss.ear.eshop.model.Category;
+import cz.cvut.kbss.ear.eshop.model.Product;
+import cz.cvut.kbss.ear.eshop.rest.util.RestUtils;
 import cz.cvut.kbss.ear.eshop.service.CategoryService;
 import cz.cvut.kbss.ear.eshop.service.ProductService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,4 +39,52 @@ public class CategoryController {
         this.productService = productService;
     }
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Category> getCategories() {
+        return service.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createCategory(@RequestBody Category category) {
+        service.persist(category);
+        LOG.debug("Created category {}.", category);
+        final HttpHeaders headers = RestUtils.createLocationHeaderFromCurrentUri("/{id}", category.getId());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    // If the parameter name matches parameter in the mapping value, it is not necessary to explicitly provide it
+    public Category getById(@PathVariable Integer id) {
+        final Category category = service.find(id);
+        if (category == null) {
+            throw NotFoundException.create("Category", id);
+        }
+        return category;
+    }
+
+    @GetMapping(value = "/{id}/products", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Product> getProductsByCategory(@PathVariable Integer id) {
+        return productService.findAll(getById(id));
+    }
+
+    @PostMapping(value = "/{id}/products", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addProductToCategory(@PathVariable Integer id, @RequestBody Product product) {
+        final Category category = getById(id);
+        service.addProduct(category, product);
+        LOG.debug("Product {} added into category {}.", product, category);
+    }
+
+    @DeleteMapping(value = "/{categoryId}/products/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeProductFromCategory(@PathVariable Integer categoryId,
+                                          @PathVariable Integer productId) {
+        final Category category = getById(categoryId);
+        final Product toRemove = productService.find(productId);
+        if (toRemove == null) {
+            throw NotFoundException.create("Product", productId);
+        }
+        service.removeProduct(category, toRemove);
+        LOG.debug("Product {} removed from category {}.", toRemove, category);
+    }
 }
